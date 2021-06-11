fbe41477a56a6369925fc8c31995a9011b3ed670
aneeshs005 <aneeshs005@gmail.com>
2021-06-11 17:55:05 +0530

    Created Layout and Added pages and index page

---
+++ Pages/Movies/Edit.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BookMyShow.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ILogger<EditModel> _logger;
+
+        public EditModel(ILogger<EditModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
